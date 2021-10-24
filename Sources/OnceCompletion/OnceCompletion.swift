dddb7d7cb7ef43bf4e10d6df61cdf4335df7a334
Baharev.Oleg <baharev.oleg@wb.ru>
2021-10-24 20:08:26 +0300

    Code optimising

--- Sources/OnceCompletion/OnceCompletion.swift
+++ Sources/OnceCompletion/OnceCompletion.swift
@@ -3,10 +3,8 @@ public final class Once<T> {
     private var value: T!
 
     public var wrappedValue: T {
-        get {
-            defer { value = nil }
-            return value // If crash here, then twice called
-        }
+        defer { value = nil }
+        return value // If crash here, then twice called
     }
 
     public var projectedValue: T? { value }
